a30c42901165cb0b5e7c65e39bd1aba9305628f1
Younghoon Jo (Eddy) <edwardevjo@gmail.com>
2018-02-14 11:51:04 -0500

    Set dashboard component

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -8,13 +8,24 @@ import { Client } from '../../models/Client';
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.css']
 })
+
 export class ClientsComponent implements OnInit {
   clients: Client[];
+  totalOwed: number;
 
   constructor(private clientService: ClientService) { }
 
   ngOnInit() {
-    this.clientService.getClients().subscribe(clients => this.clients = clients);
+    this.clientService.getClients().subscribe(clients => {
+      this.clients = clients;
+      this.getTotalOwed();
+    });
+  }
+
+  getTotalOwed() {
+    this.totalOwed = this.clients.reduce((total, client) => {
+      return total + client.balance;
+    }, 0);
   }
 
 }
