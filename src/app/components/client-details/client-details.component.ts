b4d7498c89f9d27b6d0de32d71a230482c7bfea5
Younghoon Jo (Eddy) <edwardevjo@gmail.com>
2018-02-16 11:53:30 -0500

    Set Edit and delete Client

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -3,6 +3,7 @@ import { ClientService } from '../../services/client.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { FlashMessagesService } from 'angular2-flash-messages';
 import { Client } from '../../models/Client'; 
+import { FlashMessagesModule } from 'angular2-flash-messages/module/module';
 
 @Component({
   selector: 'app-client-details',
@@ -37,4 +38,20 @@ export class ClientDetailsComponent implements OnInit {
     });
   }
 
+  updateBalance(id: string) {
+    this.clientService.updateClient(this.client);
+    this.flashMessage.show('Balance updated', {
+      cssClass: 'alert-success', timeout: 4000
+    });
+  }
+
+  onDeleteClick() {
+    if(confirm('Are you sure?')) {
+      this.clientService.deleteClient(this.client);
+      this.flashMessage.show('Client removed', {
+        cssClass: 'alert-success', timeout: 4000
+      });
+      this.router.navigate(['/']);
+    }
+  }
 }
--- src/app/components/edit-client/edit-client.component.ts
+++ src/app/components/edit-client/edit-client.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Client } from '../../models/Client'; 
+import { FlashMessagesModule } from 'angular2-flash-messages/module/module';
 
 @Component({
   selector: 'app-edit-client',
@@ -6,10 +11,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-client.component.css']
 })
 export class EditClientComponent implements OnInit {
+  id: string;
+  client: Client = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone:'',
+    balance: 0
+  }
+  disableBalanceOnEdit: boolean = true;
 
-  constructor() { }
+  constructor(    
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flashMessage: FlashMessagesService
+  ) {}
 
   ngOnInit() {
+    // Get id from url
+    this.id = this.route.snapshot.params['id'];
+    // Get client
+    this.clientService.getClient(this.id).subscribe(client => this.client = client);
+  }
+
+  onSubmit({value, valid}: {value: Client, valid: boolean}) {
+    if(!valid) {
+      this.flashMessage.show('Please fill out the form correctly', {
+        cssClass: 'alert-danger', timeout: 4000 
+      });
+    } else {
+      // Add id to client
+      value.id = this.id;
+      //update client
+      this.clientService.updateClient(value);
+      this.flashMessage.show('Client updated', {
+        cssClass: 'alert-success', timeout: 4000 
+    });
+    this.router.navigate(['/client/'+this.id]);
   }
 
 }
